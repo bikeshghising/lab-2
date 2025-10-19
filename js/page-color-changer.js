e0c00423d1c41125bca89adb250fb6d2fca08651
bikeshghising <tmgbikesh@gmail.com>
2025-10-19 09:09:17 -0400

    Funtion to change bg color and querySelector for colors

---
+++ js/page-color-changer.js
@@ -0,0 +1,14 @@
+//  Function to update the background color based on RGB input values
+window.onload = function() {
+
+    // Select the input elements for red, green, and blue color values
+    const redInput = document.querySelector('#red');
+    const greenInput = document.querySelector('#green');
+    const blueInput = document.querySelector('#blue');
+    // Select the body element of the page
+    const bodyElement = document.body;
+
+
+
+
+}
